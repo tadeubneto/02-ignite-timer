afbd77da16fac79bf691209fd101be5f8fd10a61
Tadeu <tadeubranconeto@gmail.com>
2024-12-29 12:22:15 -0300

    feat: create project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { GlobalStyle } from "./styles/global";
+export function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      
+    </div>
+  );
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #333;
+    color: #fff;
+    -webkit-font-smoothing: antialiased; //suavisar fontes
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
