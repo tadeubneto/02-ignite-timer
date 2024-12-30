ea4c09c0592cb406104a5d925b20644dab18d1e4
Tadeu <tadeubranconeto@gmail.com>
2024-12-30 11:59:11 -0300

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 export function App() {
+
   return (
-    <div>
+     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      
-    </div>
-  );
+     </ThemeProvider>
+  )
 }
