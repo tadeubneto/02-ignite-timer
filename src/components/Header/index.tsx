720fed3109e06b992192042dc72b4f81737f6db9
Tadeu <tadeubranconeto@gmail.com>
2024-12-30 11:58:40 -0300

    feat: create components header and default

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from "./style";
+
+export function Header(){
+    return (
+        <HeaderContainer>
+            <span>logo</span>
+            <nav>
+                <a href="">timer</a>
+                <a href="">home</a>
+            </nav>
+        </HeaderContainer>
+    )
+}
